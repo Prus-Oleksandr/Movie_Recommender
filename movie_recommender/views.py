49fe502d0a8d79ca7daa2fe14bcb82fb3b7a7f3e
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-05-27 12:54:28 +0300

    Rebuild index with custom manager

--- movie_recommender/views.py
+++ movie_recommender/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 
 from movie_recommender.models import Movie
+from movie_recommender.queryset_manager import MovieQuerySet
 
 
 @login_required
@@ -27,30 +28,7 @@ def index(request):
     if "session_seen_movies" not in request.session:
         request.session["session_seen_movies"] = []
 
-    watched_movies_ids = user_with_prefs.watched_movies.values_list("id", flat=True)
     session_seen_movies_ids = request.session["session_seen_movies"]
-    exclude_movies_ids = session_seen_movies_ids + watched_movies_ids
-    available_movies = Movie.objects.exclude(id__in=exclude_movies_ids) #discard watched and skipped movies
-
-    fav_genres_ids = user_with_prefs.favorite_genres.values_list("id", flat=True)
-    fav_directors_ids = user_with_prefs.favorite_directors.values_list("id", flat=True)
-    fav_actors_ids = user_with_prefs.favorite_actors.values_list("id", flat=True)
-
-    recommended_movies = available_movies.filter(
-        Q(genres__id__in=fav_genres_ids) |
-        Q(actors__id__in=fav_actors_ids) |
-        Q(directors__id__in=fav_directors_ids) # filter
-    ).annotate(match_count=Count("genres", filter=Q(genres__id__in=fav_genres_ids)) +
-                           Count("actors", filter=Q(actors__id__in=fav_actors_ids)) +
-                           Count("directors", filter=Q(directors__id__in=fav_directors_ids))
-    ).order_by("-match_count", "?").distinct() # sorter
-
-    if not recommended_movies.exists():
-        request.session["session_seen_movies"] = []
-        request.session.modified = True
-        return redirect("index")
-
-    selected_movie = recommended_movies.first()
 
     if request.method == "POST" and session_seen_movies_ids:
         currrent_movie_id = request.POST.get("movie_id")
@@ -61,14 +39,20 @@ def index(request):
                 request.session.modified = True
                 return redirect("index")
 
-            context = {
-                "selected_movie": selected_movie,
-            }
+    recommended_movies = Movie.objects.recommended_for_user(user_with_prefs,request.session["session_seen_movies"])
 
-            return render(request, "index.html", context)
+    if not recommended_movies.exists():
+        request.session["session_seen_movies"] = []
+        request.session.modified = True
+        return redirect("index")
 
-class SetPreferencesView(LoginRequiredMixin,View):
-    pass
+    context = {
+        "selected_movie": recommended_movies.first(),
+    }
+    return render(request, "index.html", context)
+
+# class SetPreferencesView(LoginRequiredMixin,View):
+
 
 
 
