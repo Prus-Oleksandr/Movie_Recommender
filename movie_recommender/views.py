eb20ca99075f2e28a63acc86166419d14b691898
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-05-27 18:05:52 +0300

    Implement manager of users preferences

--- movie_recommender/views.py
+++ movie_recommender/views.py
@@ -1,13 +1,15 @@
 import random
+from pyexpat.errors import messages
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
 from django.views import View
 
-from movie_recommender.forms import SetPreferencesForm
-from movie_recommender.models import Movie
+from movie_recommender.forms import SetPreferencesForm, SearchPreferencesForm
+from movie_recommender.models import Movie, Genre, Director, Actor
 
 User = get_user_model()
 @login_required
@@ -112,9 +114,68 @@ class SetPreferencesView(LoginRequiredMixin,View):
         }
         return render(request, self.template_name, context=context)
 
-class ManagePreferencesView(View):
+class ManagePreferencesView(LoginRequiredMixin, View):
     template_name = "manage_preferences.html"
-    pass
+    def get(self, request):
+        user = User.objects.prefetch_related(
+            "favorite_genres",
+            "favorite_directors",
+            "favorite_actors"
+        ).get(id=request.user.id)
+
+        results = []
+        search_mode = request.GET.get("search_mode")
+
+        if "query" in request.GET and "type" in request.GET:
+            form = SearchPreferencesForm(request.GET)
+            if form.is_valid():
+                query = form.cleaned_data["query"]
+                search_type = form.cleaned_data["type"]
+                if search_type == "genre":
+                    results = Genre.objects.filter(name__icontains=query)
+                elif search_type == "actor":
+                    results = Actor.objects.filter(full_name__icontains=query)
+                elif search_type == "director":
+                    results = Director.objects.filter(full_name__icontains=query)
+        context = {
+            "favorite_genres": user.favorite_genres.all(),
+            "favorite_actors": user.favorite_actors.all(),
+            "favorite_directors": user.favorite_directors.all(),
+            "results": results,
+            "search_mode": search_mode,
+        }
+        return render(request, self.template_name, context=context)
+    def post(self, request):
+        action = request.POST.get("action")
+        item_id = request.POST.get("item_id")
+
+        if not item_id or not item_id.isdigit():
+            return redirect("manage_preferences")
+
+        item_id = int(item_id)
+        user = request.user
+
+        try:
+            if action == "remove_genre":
+                user.favorite_genres.remove(item_id)
+            elif action == "remove_actor":
+                user.favorite_actors.remove(item_id)
+            elif action == "remove_director":
+                user.favorite_directors.remove(item_id)
+            elif action == "add_genre":
+                user.favorite_genres.add(item_id)
+            elif action == "add_actor":
+                user.favorite_actors.add(item_id)
+            elif action == "add_director":
+                user.favorite_directors.add(item_id)
+        except ObjectDoesNotExist:
+            messages.error(request, "Item does not exist")
+        except ValueError:
+            messages.error(request, "Invalid input")
+        return redirect("manage_preferences")
+
+
+
 
 
 
