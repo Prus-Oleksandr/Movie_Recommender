83b6a10a56557b37128b16fd65d257759d16caa3
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-05-29 08:46:34 +0300

    Add all models to admin.py

--- movie_recommender/admin.py
+++ movie_recommender/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Actor, Director, Genre, Movie, Watcher
 
-# Register your models here.
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("full_name",)
+    search_fields = ("full_name",)
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ("full_name",)
+    search_fields = ("full_name",)
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "release_year", "display_genres")
+    search_fields = ("title",)
+    filter_horizontal = ("genres", "directors", "actors")
+
+    def display_genres(self, obj):
+        return ", ".join([genre.name for genre in obj.genres.all()])
+
+    display_genres.short_description = "Genres"
+
+@admin.register(Watcher)
+class WatcherAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Preferences", {
+            "fields": ("favorite_genres", "favorite_directors", "favorite_actors", "watched_movies"),
+        }),
+    )
+    filter_horizontal = ("favorite_genres", "favorite_directors", "favorite_actors", "watched_movies", "groups", "user_permissions")
